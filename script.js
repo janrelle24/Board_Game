5fed759a8d53a75ca5e4eedc36b8607d4bfad998
janrelle24 <lubianojanrelle@gmail.com>
2025-10-13 19:36:54 +0800

    create 8x8 black and white board

---
+++ script.js
@@ -0,0 +1,17 @@
+const board = document.getElementById("board");
+
+for(let row = 0; row < 8; row++){
+    for(let col = 0; col < 8; col++){
+        const square = document.createElement("div");
+        square.classList.add("sqaure");
+
+        //alternate colors
+        if((row + col) % 2 === 0){
+            square.classList.add("white");
+        }
+        else{
+            square.classList.add("black");
+        }
+        board.appendChild(square);
+    }
+}
